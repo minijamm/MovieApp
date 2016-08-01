0ca9a0f11aff1a24f97d45b64c1a6188257b34fd
James Millichip <james@millichip.me>
2016-08-01 22:04:40 +0100

    Initial Commit

---
+++ MovieApp/App_Data/Movies.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Title] NVARCHAR(100) NOT NULL, 
+    [Director] NVARCHAR(100) NOT NULL, 
+    [DateReleased] DATETIME NOT NULL
+)
--- MovieApp/Controllers/HomeController.cs
+++ MovieApp/Controllers/HomeController.cs
@@ -3,28 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MovieApp.Models;
 
 namespace MovieApp.Controllers
 {
     public class HomeController : Controller
     {
+        // GET: Home
+        private MoviesDBEntities _db = new MoviesDBEntities();
         public ActionResult Index()
         {
-            return View();
+            return View(_db.Movies.ToList());
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
-
-            return View();
-        }
     }
 }
